2dc7f478099b28da1426551aad45193f4627f71c
kulik <kulikovn999@yandex.ru>
2021-07-16 18:08:39 +0300

    добавлена возможность двигаться вправо

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,11 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Переместить корабль вправо.
+                    self.ship.rect.x += 1
+
 
     def _update_screen(self):
         """Обновляет отображение экрана"""
