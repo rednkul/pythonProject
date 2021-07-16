d01714623e618162f8d18961b0c4990e23640c39
kulik <kulikovn999@yandex.ru>
2021-07-16 19:00:44 +0300

    Корабль умеет двигаться!

--- ship.py
+++ ship.py
@@ -10,10 +10,21 @@ class Ship():
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник.
-        self.image = pygame.image.load('images/sprite_ship.bmp.bmp')
+        self.image = pygame.image.load('images/sprite_ship.bmp')
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
