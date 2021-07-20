7233efdf534f1d1947da54e5881598a98996c12e
kulik <kulikovn999@yandex.ru>
2021-07-20 16:54:40 +0300

    Вывод одного пришельца в ЛВ угол

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Класс для управления ресурсами и повоедением игры."""
@@ -21,8 +22,11 @@ class AlienInvasion:
         pygame.display.set_caption("Alien invasion")
 
         self.ship = Ship(self)
-
         self.bullets = pygame.sprite.Group()
+
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
+
 
     def run_game(self):
 
@@ -91,9 +95,15 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-
+        self.aliens.draw(self.screen)
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
+
+    def _create_fleet(self):
+        """Создание флота вторжения"""
+        # Cоздание пришельца
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 if __name__ == '__main__':
